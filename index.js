d5ba328a3e702024eef585552558876c806c9330
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-03-03 15:43:04 +0600

    post method create done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-require("dotenv").config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
+// middlewares
+// app.use(cors());       // allow requests from frontend
+app.use(express.json()); // parse JSON bodies
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sltbrlg.mongodb.net/?appName=Cluster0`;
 
@@ -12,28 +15,43 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
+  const db = client.db("biryaniChai");
+  const divisionCollection = db.collection("divisionData");
+
+  app.post("/divisionData", async (req, res) => {
+    const data = req.body;
+    const result = await divisionCollection.insertOne(data);
+    res.send(result).status(500).send({ error: "Failed to insert data" });
+  });
+
+  // get all division data
+  // app.get("/divisionData", async (req, res) => {
+  //   try {
+  //     const divisions = await divisionCollection.find().toArray(); // fetch all
+  //     res.send(divisions);
+  //   } catch (error) {
+  //     res.status(500).send({ error: "Fetch failed" });
+  //   }
+  // });
+
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
-    await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("birYanIChai server working ....");
+});
 
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("birYanIChai server working ....")
-})
-
-app.listen(PORT,()=>{
-      console.log(`Server running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
