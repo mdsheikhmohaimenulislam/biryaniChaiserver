2e986aa3ea0b7d776077ed3a28cebee4bf319dc3
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2026-04-08 20:11:15 +0600

    Add MongoDB connection URI to index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.json()); // parse JSON bodies
 
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sltbrlg.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
